b678b6c0056355438c9f464bca813a3889d64ed8
DIPanjan198 <dipchoudhuri23@gmail.com>
2026-06-11 21:39:20 +0530

    new fix

--- Server/index.js
+++ Server/index.js
@@ -19,7 +19,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:3000",
-      "https://traveling-kpej.vercel.app"
+      "https://traveling-lac.vercel.app"
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true
